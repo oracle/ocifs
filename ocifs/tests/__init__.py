b4e1f339b75340c80c11ff14dd538a8f5f57ebd2
Allen Hosler <allen.hosler@oracle.com>
2023-08-31 19:19:39 +0100

    add tests (#20)

---
+++ ocifs/tests/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+# Copyright (c) 2021, 2023 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl/
+
+from ..core import OCIFileSystem
