20b78da9b19855561e9d8fd64e0428fe06f46e6e
Allen <allen.hosler@oracle.com>
2023-08-31 18:48:52 +0100

    add tests

--- ocifs/utils.py
+++ ocifs/utils.py
@@ -1,3 +1,8 @@
+# coding: utf-8
+# Copyright (c) 2021, 2022 Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at
+# https://oss.oracle.com/licenses/upl/
+
 import json
 import os
 
